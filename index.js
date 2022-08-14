c69ab8ed4aae85b8b78c7e218519896a25744467
k98kiana <105411392+k98kiana@users.noreply.github.com>
2022-08-14 14:00:24 -0700

    added prompt questions

--- index.js
+++ index.js
@@ -5,7 +5,73 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {//TITLE
+        type:"input",
+        message: "What is your project's title?",
+        name: "project",
+    },
+    {//DESCRIPTION-MOTIVATION
+        type:"input",
+        message: "What was the motivation for this project?",
+        name: "motivation",
+    },
+    {//DESCRIPTION-PROBLEM SOLVING
+        type:"input",
+        message: "What problem is this project solving?",
+        name: "problem-solve",
+    },
+    {//DESCRIPTION-LEARN
+        type:"input",
+        message: "What did you learn from building this project?",
+        name: "learn",
+    },
+    {//INSTALLATION
+        type:"input",
+        message: "What are the steps required to install this project?",
+        name: "installation",
+    },
+    {//USAGE
+        type:"input",
+        message: "What are the steps to use the project?",
+        name: "usage",
+    },
+    {//ADDING TO LICENSE
+        type:"input",
+        message: "What is the current year?",
+        name: "year",
+    },
+    {
+        type:"input",
+        message: "Please state your full name",
+        name: "full-name",
+    },
+    {
+        type:"confirm",
+        message: "Did anyone help you build this project?",
+        name: "contributor",
+    },
+    {
+        type:"input",
+        message: "What is the contributor's name?",
+        name: "contributer-name",
+    },
+    {
+        type:"input",
+        message: "Please provide your contibutor's Github link.",
+        name: "contributor-git",
+    },
+    {
+        type:"input",
+        message: "Please provide your Github username.",
+        name: "git-user",
+    },
+    {
+        type:"input",
+        message: "Please provide your email.",
+        name: "email",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
